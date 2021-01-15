7c6cbe0fe6436261718565ecaf62d8f905db95c5
Simon Kissane <skissane@gmail.com>
2021-01-16 05:00:43 +1100

    Skip AppleScript tests if not on macOS

--- src/test/java/blaketen/AppleScriptUtilsTest.java
+++ src/test/java/blaketen/AppleScriptUtilsTest.java
@@ -3,10 +3,18 @@ package blaketen;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.sun.jna.Memory;
+import com.sun.jna.Platform;
 import com.sun.jna.Pointer;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class AppleScriptUtilsTest {
+
+    @BeforeAll
+    public static void beforeClass() {
+        Assumptions.assumeTrue(Platform.isMac());
+    }
 
     @Test
     public void AECreateDesc() {
